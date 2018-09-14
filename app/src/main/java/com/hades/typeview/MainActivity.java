41623e3e66a84f84cf50ee8c2fedeed68a04d82b
pham.trung.huong <FRAMGIA\pham.trung.huong@framgia0387-pc.framgia.com>
2018-09-14 17:46:05 +0700

    test1

--- app/src/main/java/com/hades/typeview/MainActivity.java
+++ app/src/main/java/com/hades/typeview/MainActivity.java
@@ -3,11 +3,28 @@ package com.hades.typeview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hades.typeview.model.Passcode;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            PasscodeFragment fragment = new PasscodeFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame_container,fragment,null)
+                    .commit();
+        }
+    }
+
+    public void validatePasscode(Passcode passcode) {
+
+    }
+
+    public void updatePassCode(String value) {
+
     }
 }
